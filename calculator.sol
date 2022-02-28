95b1ead824a85dea881696b19299887e4dae23d7
aravazhimdr <aravazhimdr@gmail.com>
2022-02-28 22:43:45 +0530

    segregated private/public functions

--- calculator.sol
+++ calculator.sol
@@ -2,31 +2,52 @@ pragma solidity ^0.8.9;
 contract Calculator {
     string wt = "Welcome ";
 
-    //unint = uint256
-    //int = int256
-    function add(int a, int b) public returns (int) {
+    function strcmp(string memory a, string memory b) private pure returns (bool) {
+    return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+     }
+
+    function add(int a, int b) private returns (int) {
      int ans = int(a) + int(b);
      return ans;
     }
 
-    function sub(int a, int b) public returns (int) {
+    function sub(int a, int b) private returns (int) {
      int ans = int(a) - int(b);
      return ans;
     }
 
-    function mul(uint a, uint b) public returns (uint) {
-     uint ans = uint(a) * uint(b);
+    function mul(int a, int b) private pure returns (int) {
+     int ans = int(a) * int(b);
      return ans;
     }
 
-    function div(uint a, uint b) public returns (uint) {
-     uint ans = uint(a) / uint(b);
+    function div(int a, int b) private returns (int) {
+     int ans = int(a) / int(b);
      return ans;
     }
 
-    function mod(uint a, uint b) public returns (uint) {
-     uint ans = uint(a) % uint(b);
+    function mod(int a, int b) private pure returns (int) {
+     int ans = int(a) % int(b);
      return ans;
+    }
+
+    function calc(int a, int b, string memory ops) public returns (int){
+      require(!strcmp(ops, ""), "variable ops is empty");
+      if(strcmp(ops,"+")){
+     return add(a, b);
+      }
+      if(strcmp(ops,"-")){
+     return sub(a, b);
+      }
+      if(strcmp(ops,"/")){
+     return div(a, b);
+      }
+      if(strcmp(ops,"*")){
+     return mul(a, b);
+      }
+      if(strcmp(ops,"%")){
+     return mod(a, b);
+      }
     }
 
     function hello(string memory name) public view returns (string memory) {
